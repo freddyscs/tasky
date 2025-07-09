f570eebcf4cc02aa42e20b9a65a357f847689f6b
“freddyscs” <frrp86@gmail.com>
2025-07-10 00:14:48 +0200

    testing sec signal

--- security/py-sec.py
+++ security/py-sec.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def greet():
     name = request.args.get('name', 'World')
-    return f"<h1>Hello, {name}</h1>"
+    return f"<h1>Hello new, {name}</h1>"
 
 if __name__ == "__main__":
     app.run(debug=True)
